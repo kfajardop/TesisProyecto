22998d96cb74a55c6bc2c3ed46583962a98f2e50
kfajardop <kfajardop@miumg.edu.gt>
2025-07-28 15:15:03 -0600

    Modificación en seeder para clientes.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,10 +54,10 @@
     <!-- /.content-wrapper -->
 
     <footer class="main-footer">
-        <strong>
-            Copyright &copy; 2014-{{anioActual()}}
-            <a href="https://solucionesaltamirano.com/">Soluciones Altamirano</a>.
-        </strong>
+{{--        <strong>--}}
+{{--            Copyright &copy; 2014-{{anioActual()}}--}}
+{{--            <a href="https://solucionesaltamirano.com/">Soluciones Altamirano</a>.--}}
+{{--        </strong>--}}
         All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
             <b>Version</b> 3.0.0
