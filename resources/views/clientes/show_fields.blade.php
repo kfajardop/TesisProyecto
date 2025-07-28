a070d3bec2d2f66893030dedac5837a0048b3f43
kfajardop <kfajardop@miumg.edu.gt>
2025-07-28 16:53:42 -0600

    Implementación de datos generales para los clientes.

--- resources/views/clientes/show_fields.blade.php
+++ resources/views/clientes/show_fields.blade.php
@@ -1,42 +1,52 @@
 <!-- Primer Nombre Field -->
-<div class="col-sm-12">
+<div class="col-sm-4">
     {!! Form::label('primer_nombre', 'Primer Nombre:') !!}
     <p>{{ $cliente->primer_nombre }}</p>
 </div>
 
 <!-- Segundo Nombre Field -->
-<div class="col-sm-12">
+<div class="col-sm-4">
     {!! Form::label('segundo_nombre', 'Segundo Nombre:') !!}
     <p>{{ $cliente->segundo_nombre }}</p>
 </div>
 
 <!-- Primer Apellido Field -->
-<div class="col-sm-12">
+<div class="col-sm-4">
     {!! Form::label('primer_apellido', 'Primer Apellido:') !!}
     <p>{{ $cliente->primer_apellido }}</p>
 </div>
 
 <!-- Segundo Apellido Field -->
-<div class="col-sm-12">
+<div class="col-sm-4">
     {!! Form::label('segundo_apellido', 'Segundo Apellido:') !!}
     <p>{{ $cliente->segundo_apellido }}</p>
 </div>
 
 <!-- Dpi Field -->
-<div class="col-sm-12">
+<div class="col-sm-4">
     {!! Form::label('dpi', 'Dpi:') !!}
     <p>{{ $cliente->dpi }}</p>
 </div>
 
 <!-- Telefono Field -->
-<div class="col-sm-12">
+<div class="col-sm-4">
     {!! Form::label('telefono', 'Telefono:') !!}
     <p>{{ $cliente->telefono }}</p>
 </div>
 
 <!-- Direccion Id Field -->
-<div class="col-sm-12">
-    {!! Form::label('direccion_id', 'Direccion Id:') !!}
-    <p>{{ $cliente->direccion_id }}</p>
+<div class="col-sm-4">
+    {!! Form::label('direccion_id', 'Departamento:') !!}
+    <p>{{ $cliente->direccion->municipio->departamento->nombre }}</p>
+</div>
+
+<div class="col-sm-4">
+    {!! Form::label('direccion_id', 'Municipio:') !!}
+    <p>{{ $cliente->direccion->municipio->nombre }}</p>
+</div>
+
+<div class="col-sm-4">
+    {!! Form::label('direccion_id', 'Dirección:') !!}
+    <p>{{ $cliente->direccion->direccion }}</p>
 </div>
 
