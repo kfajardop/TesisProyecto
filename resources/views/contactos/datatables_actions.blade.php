ead1b4d7be8749333d87c3c2286383963091485e
kfajardop <kfajardop@miumg.edu.gt>
2025-09-22 20:49:00 -0600

    Mejoras en los botones de acción de la tabla contactos

--- resources/views/contactos/datatables_actions.blade.php
+++ resources/views/contactos/datatables_actions.blade.php
@@ -1,18 +1,18 @@
 @can('Ver Contactos')
-    <a href="{{ route('contactos.show', $id) }}" data-toggle="tooltip" title="Ver" class='btn btn-sm btn-outline-primary'>
-        <i class="fa fa-eye"></i>
+    <a href="{{ route('contactos.show', $id) }}" data-toggle="tooltip" title="Ver" class="mx-2">
+        <i class="fa fa-eye text-primary" ></i>
     </a>
     @endcan
 
 @can('Editar Contactos')
-    <a href="{{ route('contactos.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-sm btn-outline-warning'>
-        <i class="fa fa-edit"></i>
+    <a href="{{ route('contactos.edit', $id) }}" data-toggle="tooltip" title="Editar" class="mx-2" >
+        <i class="fa fa-edit text-warning"></i>
     </a>
 @endcan
 
 @can('Eliminar Contactos')
-    <a href="#" onclick="deleteItemDt(this)" data-id="{{ $id }}" data-toggle="tooltip" title="Eliminar" class='btn btn-sm btn-outline-danger'>
-        <i class="fa fa-trash-alt"></i>
+    <a href="#" onclick="deleteItemDt(this)"  data-id="{{ $id }}" data-toggle="tooltip" title="Eliminar" class="mx-2">
+        <i class="fa fa-trash-alt text-danger"></i>
     </a>
 
 
